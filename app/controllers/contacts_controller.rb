9263d54ff9d158db48349683e6fb5c4feda0ead7
Amihai Shapira <koooot@gmail.com>
2016-01-22 08:47:52 +0000

    added actionmailer settings

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,21 +4,21 @@ class ContactsController < ApplicationController
     end
     
     def create
-        @contact = Contact.new(contact_params)
+      @contact = Contact.new(contact_params)
+      
+      if @contact.save
+        name = params[:contact][:name]
+        email = params[:contact][:email]
+        body = params[:contact][:comments]
         
-        if @contact.save
-            name = params[:contact][:name]
-            email = params[:contact][:email]
-            body = params[:contact][:comments]
-            
-            ContactMailer.contact_email(name, email, body).deliver
-            
-            flash[:success] = 'Message sent'
-            redirect_to new_contact_path
-        else 
-            flash[:danger] = 'Error. Message was not sent :('
-            redirect_to new_contact_path
-        end
+        ContactMailer.contact_email(name, email, body).deliver
+        
+        flash[:success] = 'Message sent.'
+        redirect_to new_contact_path
+      else
+        flash[:danger] = 'Error occured, message has not been sent.'
+        redirect_to new_contact_path
+      end
     end
     
     private
